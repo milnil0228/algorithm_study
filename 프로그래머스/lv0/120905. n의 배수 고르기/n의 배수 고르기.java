b4e594b687db6c095d267313d7bd2e542dfbd7b0
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-05 12:35:34 +0900

    Update n의 배수 고르기.java

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -11,3 +11,9 @@ class Solution {
         return answer.stream().mapToInt(x -> x).toArray();
     }
 }
+// public int[] solution(int n, int[] numlist) {    //stream 공부하면서 돌려본 
+        int[] answer = numlist;
+        answer = Arrays.stream(numlist).filter(num -> num % n == 0).toArray();
+        return answer;
+   }
+// return Arrays.stream(numlist).filter(num -> num % n == 0).toArray(); //가장 간단한 답안
