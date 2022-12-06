a4fc99de0bf8ca8fc06dfc9d9cb3a94729f92a36
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-06 09:50:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,18 +1,10 @@
 class Solution {
     public int solution(int num) {
-        int count = 0;
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-            }
-            else if (num % 2 == 1) {
-                num = num * 3 + 1;
-            }
-            count++;
-            if (count == 500) {
-                return -1;
-            }
-        }
-    return count;
+        long n = (long)num;
+    for(int i =0; i<500; i++){      
+      if(n==1) return i;
+      n = (n%2==0) ? n/2 : n*3+1;            
+    }
+    return -1;
     }
 }
