3d41613a0a40c2fdb4dce9c6f3001be8625d52e6
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-07 16:18:48 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -9,7 +9,7 @@ class Solution {
             }
         }
         else {
-            int min = total / num - num / 2;
+            int min = total / num - num / 2; //테스트 해 본 결과 num이 int로 선언되어 있어 num/2는 정수형으로만 나옴
             for (int j = 0; j < num; j++) {
                 answer[j] = min;
                 min++;
