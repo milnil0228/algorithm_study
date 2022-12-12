2c930e7d0f13fbb5e57ef1a85a800a7a0ef902a6
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-12 11:53:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -19,3 +19,10 @@ class Solution {
         return quiz;
     }
 }
+
+// for(int i=0; i<quiz.length; i++){
+//             String[] text = quiz[i].split(" ");
+//             int result = Integer.parseInt(text[0]) + ( Integer.parseInt(text[2]) * ( text[1].equals("+") ? 1:-1) );
+//             quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
+//         }
+//         return quiz;
