1a917936695f970de56599a92918fb9d77a497a1
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-12 15:32:48 +0900

    [level 0] Title: k의 개수, Time: 1.96 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        for (; i <= j; i++) {
-            if (Integer.toString(i).contains(Integer.toString(k))) {
-                for (int l = 0; l < Integer.toString(i).length(); l++) {
-                    if (Integer.toString(i).charAt(l) == (char)(k+'0')) {
-                        answer++;
-                    }
-                }
+
+        for (int num = i; num <= j; num++){
+            int tmp = num;
+            while (tmp != 0){
+                if (tmp % 10 == k)
+                    answer++;
+                tmp /= 10;
             }
         }
         return answer;
