b9a715c88360510bbe65554598a98267015efd70
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-14 10:34:26 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int solution(int[][] dots) {
-        for (int i = 0; i < 3; i++) {
+    public int solution(int[][] dots) {     //dots이 정수형이므로 나누기하였을 때 정상적으로 계산되지 않음
+        for (int i = 0; i < 3; i++) {       //기울기의 분모를 양쪽에 곱해주어 정수로 나타냄
             if ((dots[3][0] - dots[i][0]) * (dots[(i + 1) % 3][1] - dots[(i + 2) % 3][1])
                == (dots[(i + 1) % 3][0] - dots[(i + 2) % 3][0]) * (dots[3][1] - dots[i][1])) {
                 return 1;
