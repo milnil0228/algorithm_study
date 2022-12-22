db0df82ace6ac7a0c009cddd27e526af237814a2
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-22 15:15:52 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int[] common) {
         if (common[1] - common[0] == common[2] - common[1])
             return common[common.length - 1] + common[1] - common[0];
-        return common[common.length - 1] * (common[1] / common[0]);
+        return common[common.length - 1] * (common[1] / common[0]); //common[0]이 0이라면? 나누기 불가능.
     }
 }
