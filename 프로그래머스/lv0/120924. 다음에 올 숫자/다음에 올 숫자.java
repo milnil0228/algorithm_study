e29b4f520f1d77bdd3b1b7fcde9feae7280df144
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-22 15:44:01 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] common) {
-        if (common[1] - common[0] == common[2] - common[1])
+        if (common[1] - common[0] == common[2] - common[1])  //등비인지를 먼저 확인할 경우 나누기 0이 되는 경우가 발생 가능함
             return common[common.length - 1] + common[1] - common[0];
-        return common[common.length - 1] * (common[1] / common[0]); //common[0]이 0이라면? 나누기 불가능.
+        return common[common.length - 1] * (common[1] / common[0]);
     }
 }
