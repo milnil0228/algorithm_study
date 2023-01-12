2da2280857337e22ebd50809a2ffa45ae0518e07
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2023-01-12 17:21:33 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,3 +1,6 @@
-SELECT i.animal_id, i.name from animal_ins i left join animal_outs o on i.animal_id = o.animal_id
+SELECT i.animal_id, i.name
+from animal_ins i
+    left join animal_outs o
+           on i.animal_id = o.animal_id
 where i.datetime - o.datetime > 0
 order by i.datetime
