abd633ea3ca08a0c11dfc0369492bfeb92903b11
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2023-01-12 16:57:19 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,6 @@
-SELECT i.name, i.datetime from animal_outs o
-    right join animal_ins i
-           on o.animal_id = i.animal_id
+SELECT i.name, i.datetime from animal_ins i
+    left join animal_outs o
+           on i.animal_id = o.animal_id
 where o.animal_id is null
 order by i.datetime
 limit 3
