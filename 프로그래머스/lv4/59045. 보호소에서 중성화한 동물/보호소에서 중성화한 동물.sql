26584716abbd42fb40051c67365b3f01ffd9a891
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2023-01-12 17:06:20 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,3 +1,8 @@
-SELECT animal_id, animal_type, name from animal_ins i
-where sex_upon_intake like 'intact%' and animal_id in (select animal_id from animal_outs where sex_upon_outcome like 'Spayed%' or sex_upon_outcome like 'Neutered%')
+SELECT animal_id, animal_type, name
+from animal_ins i
+where sex_upon_intake like 'intact%' and
+      animal_id in (select animal_id
+                    from animal_outs
+                    where sex_upon_outcome like 'Spayed%'
+                       or sex_upon_outcome like 'Neutered%')
 order by animal_id
