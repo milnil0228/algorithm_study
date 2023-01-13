651ebc27f9fd7f0b4241373cafe14e7ee8d4b232
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2023-01-13 14:29:20 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,8 +1,6 @@
-SELECT *
-FROM (SELECT FOOD_TYPE,REST_ID,REST_NAME,FAVORITES
-      FROM REST_INFO
-      ORDER BY FAVORITES DESC
-      LIMIT 20000) SUB
+SELECT main.FOOD_TYPE,main.REST_ID,main.REST_NAME,main.FAVORITES
+      FROM (select food_type, max(favorites) as max from REST_INFO group by food_type) sub
+      inner join rest_info main on sub.food_type = main.food_type and sub.max = main.favorites
 GROUP BY FOOD_TYPE
 ORDER BY FOOD_TYPE DESC
 
@@ -13,3 +11,16 @@ ORDER BY FOOD_TYPE DESC
 #      LIMIT 112345) SUB
 # GROUP BY FOOD_TYPE
 # ORDER BY FOOD_TYPE DESC
+
+# SELECT 
+# r.FOOD_TYPE,
+# r.REST_ID,
+# r.REST_NAME,
+# r.FAVORITES
+# FROM (
+#     select FOOD_TYPE, MAX(FAVORITES) AS maxfv
+#     from REST_INFO group by FOOD_TYPE
+# )  AS X
+# inner join REST_INFO as r 
+# ON r.FOOD_TYPE = x.FOOD_TYPE and r.FAVORITES = x.maxfv
+# ORDER BY FOOD_TYPE DESC ;
