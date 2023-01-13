4c2e9c13bb2c634bdc73edb6841a75256f389847
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2023-01-13 12:16:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 10.19 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int n) {
         String Ternary = "";
-        int temp = 0;
         while (n > 0) {
-            temp = n % 3;
+            Ternary += n % 3;
             n /= 3;
-            Ternary += temp;
         }
         return Integer.parseInt(Ternary,3);
     }
